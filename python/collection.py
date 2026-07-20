9fa1837f45ebdbc3e353ebb95504cede1fa81a05
monisha4evr <monisha.it1510@gmail.com>
2026-07-20 16:03:13 +0530

    Collection - set,dict, String

--- python/collection.py
+++ python/collection.py
@@ -57,3 +57,44 @@ a=(1,2,3,5,2,4,4,4,44,4,4)
 print(type(a))
 print(a.count(4))
 print(a.index(9))
+
+
+# set
+a={1,2,3}
+b={1,4,5}
+print(type(a))
+print(a.union(b))
+print(a|b)
+
+print(a.intersection(b))
+print(a&b)
+
+print(a.difference(b))
+print(a-b)
+
+print(a.symmetric_difference(b))
+print(a^b)
+
+#dict
+
+z={"name":"apple","color":"red"}
+print(type(z))
+z['price']="200"
+print(z)
+z['color']="Green"
+print(z['color'])
+
+
+z={"name":"apple","color":"red"}
+for i in z.keys():
+    print(i)
+
+for j in z.values():
+    print(j)
+
+for i,j in z.items():
+    print("Value",j)
+
+
+
+
---
+++ python/string.py
@@ -0,0 +1,52 @@
+a=100
+b=20
+c=15
+
+if a>b and a>c:
+    print(" a is Bigger ")
+elif b>c:
+    print("B is Bigger")
+else:
+    print("C is Bigger")
+
+a=51
+print("Even" if a%2==0 else "Odd")
+
+# String
+
+a="Flower"
+print(a[3])
+
+# Slicing
+#[start:end:step]
+
+b="I am learning Python"
+print(b[2:4])
+print(b[:5])
+print(b[5:10])
+print(b[::2])
+print(b[::-1])
+
+
+c=" flowers "
+print(c[1:4])
+print(c.capitalize())
+print(len(c))
+print(c.find('l'))
+print(c.index('l'))
+print(c)
+print(c.strip())
+
+strng="madamss"
+#city
+rev=strng[::-1]
+print(strng)
+print(rev)
+if strng == rev:
+    print("Palindrome")
+else:
+    print("not a Palindrome")
+
+
+a="Hello "*5
+print(a)
